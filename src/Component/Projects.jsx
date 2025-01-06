04da82782d8eb8e04bd401562216b0017514ca70
shrihari1363@gmail.com <105267301+ShriHari1307@users.noreply.github.com>
2025-01-06 16:13:41 +0530

    included project repo links

--- src/Component/Projects.jsx
+++ src/Component/Projects.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { useState, useEffect } from "react";
+import { useState,useEffect } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 export default function Projects({ isDarkMode }) {
@@ -9,11 +9,21 @@ export default function Projects({ isDarkMode }) {
       description: [
         "This project involved deploying a complete 3-tier architecture application on AWS EKS.",
         "The application was containerized using Docker, and Dockerfiles were created for each tier.",
-        "These containers were then deployed and orchestrated within AWS EKS, ensuring high availability and scalability. The architecture comprises a frontend, backend, and database layer, each deployed as separate services, allowing seamless communication and efficient management of the application on the cloud."
+        "These containers were then deployed and orchestrated within AWS EKS, ensuring high availability and scalability.",
+        " The architecture comprises a frontend, backend, and database layer, each deployed as separate services, allowing seamless communication and efficient management of the application on the cloud.",
       ],
-      tools: ["Docker", "AWS EKS", "Kubernetes", "AWS EC2", "Git", "Maven","VSCode","AWS RDS","Spring boot"],
-      liveDemo: "https://example.com/live-demo/aws-cloud-adoption",
-      github: "https://github.com/yourusername/aws-cloud-adoption",
+      tools: [
+        "Docker",
+        "AWS EKS",
+        "Kubernetes",
+        "AWS EC2",
+        "Git",
+        "Maven",
+        "VSCode",
+        "AWS RDS",
+        "Spring boot",
+      ],
+      github: "https://github.com/ShriHari1307/EKSThreeTier",
       image: "kubernetes-proj.jpg",
     },
     {
@@ -24,19 +34,35 @@ export default function Projects({ isDarkMode }) {
         "Additionally, it offers a user-friendly interface that allows seamless interaction with agent management tasks.",
         "The system supports real-time updates to ensure that the agent data is always up-to-date, reducing the need for manual interventions.",
       ],
-      tools: ["React", "Spring Boot", "MySQL", "Bootstrap", "VSCode", "Git", "JWT", "Maven"],
-      liveDemo: "https://example.com/live-demo/redhat-automation",
-      github: "https://github.com/yourusername/redhat-automation",
+      tools: [
+        "React",
+        "Spring Boot",
+        "MySQL",
+        "Bootstrap",
+        "VSCode",
+        "Git",
+        "Maven",
+      ],
+      github: "https://github.com/ShriHari1307/InsuranceAgentManagement",
       image: "react-project.jpeg",
-    },    
+    },
     {
       title: "Complete CICD Pipeline using Jenkins",
       description: [
         "This project involved setting up a complete CI/CD pipeline using Jenkins, integrated with code quality tools like SonarQube and Nexus OSS repository.",
         "The pipeline automates the build, test, and deployment processes, ensuring faster and more reliable software delivery. Jenkins handles the automation, while SonarQube ensures code quality by analyzing and reporting on potential issues.",
-        "Nexus OSS is used to store and manage the build artifacts, and the entire pipeline is deployed on AWS EKS, enabling efficient scaling and management of the application in a containerized environment."
+        "Nexus OSS is used to store and manage the build artifacts, and the entire pipeline is deployed on AWS EKS, enabling efficient scaling and management of the application in a containerized environment.",
       ],
-      tools: ["Jenkins", "SonarQube", "Nexus OSS", "Docker", "AWS EKS", "Git Bash", "Maven","AWS EC2"],
+      tools: [
+        "Jenkins",
+        "SonarQube",
+        "Nexus OSS",
+        "Docker",
+        "AWS EKS",
+        "Git Bash",
+        "Maven",
+        "AWS EC2",
+      ],
       liveDemo: "https://example.com/live-demo/aws-cloud-quest",
       github: "https://github.com/yourusername/aws-cloud-quest",
       image: "pipeline.png",
@@ -69,11 +95,17 @@ export default function Projects({ isDarkMode }) {
   return (
     <section
       id="projects"
-      className={`min-h-screen p-6 ${isDarkMode ? "bg-black text-white" : "bg-gradient-to-r from-[#f0f4f8] to-[#e1e8ee] text-black"}`}
+      className={`min-h-screen p-6 ${
+        isDarkMode
+          ? "bg-black text-white"
+          : "bg-gradient-to-r from-[#f0f4f8] to-[#e1e8ee] text-black"
+      }`}
     >
       <div className="container mx-auto">
         <h2
-          className={`text-4xl font-bold text-center mb-12 ${isDarkMode ? "text-white" : "text-black"}`}
+          className={`text-4xl font-bold text-center mb-12 ${
+            isDarkMode ? "text-white" : "text-black"
+          }`}
         >
           Projects
         </h2>
@@ -96,7 +128,9 @@ export default function Projects({ isDarkMode }) {
               {projects.map((project, index) => (
                 <div
                   key={index}
-                  className={`flex-shrink-0 w-full rounded-lg shadow-lg p-10 ${isDarkMode ? "bg-gray-800" : "bg-white"}`}
+                  className={`flex-shrink-0 w-full rounded-lg shadow-lg p-10 ${
+                    isDarkMode ? "bg-gray-800" : "bg-white"
+                  }`}
                 >
                   <div className="flex flex-col lg:flex-row items-center justify-between">
                     <div className="w-full lg:w-2/5 h-72 overflow-hidden mb-6 lg:mb-0">
@@ -107,10 +141,18 @@ export default function Projects({ isDarkMode }) {
                       />
                     </div>
                     <div className="w-full lg:w-3/5 lg:pl-6">
-                      <h3 className={`text-3xl font-semibold mb-4 ${isDarkMode ? "text-white" : "text-black"}`}>
+                      <h3
+                        className={`text-3xl font-semibold mb-4 ${
+                          isDarkMode ? "text-white" : "text-black"
+                        }`}
+                      >
                         {project.title}
                       </h3>
-                      <div className={`mt-4 text-base ${isDarkMode ? "text-gray-300" : "text-gray-700"} text-justify`}>
+                      <div
+                        className={`mt-4 text-base ${
+                          isDarkMode ? "text-gray-300" : "text-gray-700"
+                        } text-justify`}
+                      >
                         <ul>
                           {project.description.map((desc, index) => (
                             <li key={index} className="mb-2">
@@ -124,14 +166,16 @@ export default function Projects({ isDarkMode }) {
                         </p>
                       </div>
                       <div className="mt-4 flex space-x-6">
-                        <a
-                          href={project.liveDemo}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="px-8 py-3 bg-green-500 text-white rounded-lg transition-all duration-300 hover:bg-green-600"
-                        >
-                          Live Demo
-                        </a>
+                        {project.liveDemo && (
+                          <a
+                            href={project.liveDemo}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="px-8 py-3 bg-green-500 text-white rounded-lg transition-all duration-300 hover:bg-green-600"
+                          >
+                            Live Demo
+                          </a>
+                        )}
                         <a
                           href={project.github}
                           target="_blank"
